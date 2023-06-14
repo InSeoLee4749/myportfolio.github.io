2d58b44674c5448379479ad77d7a46b99dedbfa5
InSeoLee4749 <131342595+InSeoLee4749@users.noreply.github.com>
2023-06-14 16:54:08 +0900

    my portfolio 첫 시안 제작

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $(".nav-link").click(function () {
+        $(".nav-link").removeClass("act");
+        $(this).addClass("act");
+    });
+
+    $(window).scroll(function () {
+        //스크롤을 하면
+        if ($(window).scrollTop() > 80) {
+            //스크롤의 위치가 80보다 커지면
+            $("body").addClass("scroll"); //body에 scroll클래스 삽입하라
+        } else {
+            $("body").removeClass("scroll");
+        }
+    });
+});
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
